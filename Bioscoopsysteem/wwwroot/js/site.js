cf84a381d7a41357f268240fbba27eea90914f89
qbus167 <qvnguyen75@gmail.com>
2021-03-18 09:12:31 +0100

    [ADD] Iss24

--- Bioscoopsysteem/wwwroot/js/site.js
+++ Bioscoopsysteem/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function DisplayGoogleMap() {
+
+    //Set the Latitude and Longitude of the Map
+    var myAddress = new google.maps.LatLng(51.58425641517101, 4.798083988657903);
+
+    //Create Options or set different Characteristics of Google Map
+    var mapOptions = {
+        center: myAddress,
+        zoom: 15,
+        minZoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //Display the Google map in the div control with the defined Options
+    var map = new google.maps.Map(document.getElementById("myDiv"), mapOptions);
+
+    //Set Marker on the Map
+    var marker = new google.maps.Marker({
+        position: myAddress,
+        animation: google.maps.Animation.DROP,
+    });
+
+    marker.setMap(map);
+}
+
+DisplayGoogleMap();
+
